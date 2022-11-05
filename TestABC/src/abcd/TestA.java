c569c0b86281de0c9090b31da12a53df0ab5dbd2
Daniel <Daniel@LAPTOP-IV3UDAB9>
2022-11-05 08:44:30 +0530

    nnnnnn

---
+++ TestABC/src/abcd/TestA.java
@@ -0,0 +1,17 @@
+package abcd;
+
+public class TestA {
+
+	public static void main(String[] args) {
+
+			System.out.println("srinivas");
+			System.out.println("etst");
+			System.out.println("srinivas");
+			System.out.println("etst");
+			System.out.println("srinivas");
+			System.out.println("etst");
+
+
+	}
+
+}
